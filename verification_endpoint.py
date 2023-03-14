7b4d039ecc9b845c0fe7ed68911b23201bbac4fe
tpmccarthy1 <tommccarthy@Toms-MacBook-Pro.local>
2023-03-14 15:14:49 -0400

    update

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,12 +11,12 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
-    sig = content["sig"][2:]
+    sig = content["sig"]
     payload = content["payload"]
     result = False
     if (payload["platform"] == 'Ethereum'):
         print(sig)
-        if eth_account.Account.recover_message(json.dumps(payload).encode('utf-8'),signature=sig.hex()) == payload["pk"]:
+        if eth_account.Account.recover_message(json.dumps(payload).encode('utf-8'),signature=int(sig, 0).hex()) == payload["pk"]:
             result = True
     if (payload["platform"] == 'Algorand'):
         if algosdk.util.verify_bytes(json.dumps(payload).encode('utf-8'),sig,payload["pk"]):
