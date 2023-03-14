60de8673c84cfea317ecfa3983dfc0d7238f2f9b
tpmccarthy1 <tommccarthy@Toms-MacBook-Pro.local>
2023-03-14 14:15:45 -0400

    update

--- verification_endpoint.py
+++ verification_endpoint.py
@@ -11,7 +11,7 @@ app.url_map.strict_slashes = False
 @app.route('/verify', methods=['GET','POST'])
 def verify():
     content = request.get_json(silent=True)
-
+    print(content)
     #Check if signature is valid
     result = True #Should only be true if signature validates
     return jsonify(result)
